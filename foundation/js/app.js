f415ce8c01dc9ab47b6356ac963ebad2fdaf0ee6
dongshaohan <234714973@qq.com>
2016-10-15 21:27:02 +0800

    test

--- foundation/js/app.js
+++ foundation/js/app.js
@@ -452,7 +452,7 @@ $(function () {
 
             setTimeout(function () {
                 self.$el.html( self.tpl({data: data}) );
-            }, 450);
+            }, 500);
         },
         remove: function () {
             this.$el.off('click');
