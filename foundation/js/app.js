4fc2c861237429d795fdc319e6de04610f746d2a
dongshaohan <234714973@qq.com>
2016-10-16 11:19:41 +0800

    test

--- foundation/js/app.js
+++ foundation/js/app.js
@@ -61,19 +61,14 @@ $(function () {
                 nonceStr: res.nonceStr,
                 signature: res.signature,
                 jsApiList: [
-                    'openEnterpriseChat'
+                    'openEnterpriseChat',
+                    'hideOptionMenu'
                 ]
             });
         });
         
         wx.ready(function () {
-            wx.checkJsApi({
-                jsApiList: ['openEnterpriseChat'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
-                success: function(res) {
-                    // 以键值对的形式返回，可用的api值true，不可用为false
-                    // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
-                }
-            });
+            wx.hideOptionMenu();
         });
     };
 
@@ -376,7 +371,7 @@ $(function () {
                 alert('点击成功')
                 wx.openEnterpriseChat({
                     userIds: data.userid, 
-                    groupName: "",  //
+                    groupName: "", 
                     success: function(res) {
                         alert('打开会话成功')
                     },
